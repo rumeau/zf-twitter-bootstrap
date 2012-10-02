907e1147c804c858c1d87aae3354e5ed434faed4
Jean Rumeau <rumeau@gmail.com>
2012-10-02 00:17:08 -0300

    Implementado elemento Search

---
+++ src/ZfTwitterBootstrap/Form/Element/Search.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ZfTwitterBootstrap\Form\Element;
+
+use ZfTwitterBootstrap\Form\Element\Text;
+
+class Search extends Text
+{}
--- src/ZfTwitterBootstrap/Form/View/Helper/FormSearch.php
+++ src/ZfTwitterBootstrap/Form/View/Helper/FormSearch.php
@@ -5,13 +5,4 @@ namespace ZfTwitterBootstrap\Form\View\Helper;
 use Zend\Form\View\Helper\FormSearch as ZendFormSearchHelper;
 
 class FormSearch extends ZendFormSearchHelper
-{
-    /**
-     * Seed attributes
-     *
-     * @var array
-     */
-    protected $attributes = array(
-            'type' => 'text',
-    );
-}
+{}
