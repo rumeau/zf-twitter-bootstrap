40e914672738fd335ba4105f789666a88861ec41
Jean Rumeau <rumeau@gmail.com>
2012-10-02 00:13:47 -0300

    Especificado tipo de elemento Search
    falta definir su clase css

--- src/ZfTwitterBootstrap/Form/View/Helper/FormSearch.php
+++ src/ZfTwitterBootstrap/Form/View/Helper/FormSearch.php
@@ -5,4 +5,13 @@ namespace ZfTwitterBootstrap\Form\View\Helper;
 use Zend\Form\View\Helper\FormSearch as ZendFormSearchHelper;
 
 class FormSearch extends ZendFormSearchHelper
-{}
+{
+    /**
+     * Seed attributes
+     *
+     * @var array
+     */
+    protected $attributes = array(
+            'type' => 'text',
+    );
+}
